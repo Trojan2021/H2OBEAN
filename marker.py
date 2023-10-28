f55936b9f181d3bc707fb1be980bd95cc054c6ab
AnotherMichael8 <130487522+AnotherMichael8@users.noreply.github.com>
2023-10-28 17:33:28 -0400

    Backend Foundation

---
+++ marker.py
@@ -0,0 +1,14 @@
+class Marker:
+    
+    def __init__(self, ppmValue, long, lat, id):
+        self.ppmValue = ppmValue
+        self.long = long
+        self.lat = lat
+        self.id = id
+    
+    def insertMarker(marker):
+        pass
+    def getMarkerByID(id):
+        pass
+    
+    
