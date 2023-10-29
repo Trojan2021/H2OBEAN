06c0f3c07364a4b44072954daf576c4ab00a0b80
Trojan2021 <sawyereramsey@gmail.com>
2023-10-29 06:35:10 -0400

    New Display

--- display.ino/display.ino.ino
+++ display.ino/display.ino.ino
@@ -1,3 +1,9 @@
+#include <Arduino.h>
+#include <Adafruit_GFX.h>  // Core graphics library
+#include "Adafruit_MAX1704X.h"
+#include <Adafruit_ST7789.h>
+#include <Fonts/FreeSans12pt7b.h>
+
 #define TdsSensorPin A0
 #define VREF 5.0           // analog reference voltage(Volt) of the ADC
 #define SCOUNT 30          // sum of sample point
@@ -6,12 +12,35 @@ int analogBufferTemp[SCOUNT];
 int analogBufferIndex = 0, copyIndex = 0;
 float averageVoltage = 0, tdsValue = 0, temperature = 25;
 
+Adafruit_MAX17048 lipo;
+Adafruit_ST7789 display = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+GFXcanvas16 canvas(240, 135);
+
 void setup() {
   Serial.begin(115200);
+  delay(100);
   pinMode(TdsSensorPin, INPUT);
+
+  display.init(135, 240);  // Init ST7789 240x135
+  display.setRotation(3);
+  canvas.setFont(&FreeSans12pt7b);
+  canvas.setTextColor(ST77XX_WHITE);
+
+  if (!lipo.begin()) {
+    // Serial.println(F("Couldnt find Adafruit MAX17048?\nMake sure a battery is plugged in!"));
+    while (1) delay(10);
+  }
+
+  pinMode(0, INPUT_PULLUP);
+  pinMode(1, INPUT_PULLDOWN);
+  pinMode(2, INPUT_PULLDOWN);
 }
 
+uint8_t j = 0;
+
 void loop() {
+
   static unsigned long analogSampleTimepoint = millis();
   if (millis() - analogSampleTimepoint > 40U)  //every 40 milliseconds,read the analog value from the ADC
   {
@@ -30,13 +59,35 @@ void loop() {
     float compensationCoefficient = 1.0 + 0.02 * (temperature - 25.0);                                                                                                                //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
     float compensationVolatge = averageVoltage / compensationCoefficient;                                                                                                             //temperature compensation
     tdsValue = (133.42 * compensationVolatge * compensationVolatge * compensationVolatge - 255.86 * compensationVolatge * compensationVolatge + 857.39 * compensationVolatge) * 0.5;  //convert voltage value to tds value
-    //Serial.print("voltage:");
-    //Serial.print(averageVoltage,2);
-    //Serial.print("V ");
-    Serial.print("TDS Value:");
-    Serial.print(tdsValue, 0);
-    Serial.println("ppm");
+    Serial.println(tdsValue, 0);
   }
+
+  if (j % 2 == 0) {
+    canvas.fillScreen(ST77XX_BLACK);
+    canvas.setCursor(0, 17);
+    canvas.setTextColor(ST77XX_RED);
+    canvas.println("H2OBEAN");
+    canvas.setTextColor(ST77XX_YELLOW);
+    canvas.println("Water Quality Tester");
+    canvas.setTextColor(ST77XX_GREEN);
+    canvas.print("Battery: ");
+    canvas.setTextColor(ST77XX_WHITE);
+    canvas.print(lipo.cellVoltage(), 1);
+    canvas.print(" V  /  ");
+    canvas.print(lipo.cellPercent(), 0);
+    canvas.println("%");
+    canvas.setTextColor(ST77XX_BLUE);
+    canvas.print("TDS Value: ");
+    canvas.setTextColor(ST77XX_WHITE);
+    canvas.print(tdsValue);
+    canvas.println("ppm");
+    display.drawRGBBitmap(0, 0, canvas.getBuffer(), 240, 135);
+    pinMode(TFT_BACKLITE, OUTPUT);
+    digitalWrite(TFT_BACKLITE, HIGH);
+  }
+
+  delay(10);
+  return;
 }
 
 int getMedianNum(int bArray[], int iFilterLen) {
